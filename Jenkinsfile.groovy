c6fd83a90c4787d6ae0aa1b87c5cb135bba1ee0a
Rob <brem_robert@hotmail.com>
2018-04-25 20:17:10 +0200

    try in a node container

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,6 +4,7 @@ podTemplate(label: 'mypod', containers: [
         containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat'),
         containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.0', command: 'cat', ttyEnabled: true),
         containerTemplate(name: 'curl', image: 'khinkali/jenkinstemplate:0.0.3', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'node', image: 'node', ttyEnabled: true, command: 'cat')
 ],
         volumes: [
                 hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
@@ -28,14 +29,17 @@ podTemplate(label: 'mypod', containers: [
             def npmHome = tool 'npm'
             env.PATH = "${npmHome}/bin:${env.PATH}"
 
-            sh 'npm install'
-            sh 'npm run mlbuild'
-            sh 'npm run build'
+            container('node') {
+                sh 'npm install'
+                sh 'npm run mlbuild'
+                sh 'npm run build'
+            }
 
             env.VERSION = semanticReleasing()
             currentBuild.displayName = env.VERSION
 
             sh "git config user.email \"jenkins@khinkali.ch\""
+
             sh "git config user.name \"Jenkins\""
             sh "git tag -a ${env.VERSION} -m \"${env.VERSION}\""
             withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
