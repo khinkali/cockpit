5732dec055daaa53d41b9633794e08da1544a846
Rob <brem_robert@hotmail.com>
2018-05-17 21:46:25 +0200

    [REFAC] keyclaok interceptor and axios interceptor

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-/* Import css */
 import 'font-awesome/css/font-awesome.css';
-import './index.css'; 
-
-/* Import javascript */
+import './index.css';
 import 'bootstrap';
-//import './SbAdmin/js/sb-admin';
+import Keycloak from "./keycloak.js";
+import axios from "axios";
 
 import App from './App/AppContainer.bs';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const kc = Keycloak('/keycloak.json');
+kc.init({onLoad: 'login-required'}).success(authenticated => {
+    if (authenticated) {
+        ReactDOM.render(<App/>, document.getElementById('root'));
+        registerServiceWorker();
+    }
+});
+
+axios.interceptors.request.use(config => {
+    return refreshToken().then(() => {
+        config.headers.Authorization = 'Bearer ' + kc.token;
+        return Promise.resolve(config)
+    }).catch(() => {
+        kc.login();
+    })
+});
+
+// need to wrap the KC "promise object" into a real Promise object
+const refreshToken = (minValidity = 5) => {
+    return new Promise((resolve, reject) => {
+        kc.updateToken(minValidity)
+            .success(() => resolve())
+            .error(error => reject(error))
+    });
+};
