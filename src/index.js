db286f8c9f2502fcd4f011ddb7ddf44fd20e674c
Bifunctor <bifunctor@khinkali.com>
2018-05-19 22:02:19 +0200

    Reorganize folders

--- src/index.js
+++ src/index.js
@@ -1,37 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'font-awesome/css/font-awesome.css';
-import './index.css';
+
+// js
 import 'bootstrap';
+
+// styles
+import './index.scss';
+
 import App from './App/AppContainer.bs';
-import registerServiceWorker from './registerServiceWorker';
-//import Keycloak from 'keycloak-js';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
-
-/*const kc = Keycloak();
-kc.init({onLoad: 'login-required'}).success(authenticated => {
-    if (authenticated) {
-        ReactDOM.render(<App/>, document.getElementById('root'));
-        registerServiceWorker();
-    }
-});
-
-axios.interceptors.request.use(config => {
-    return refreshToken().then(() => {
-        config.headers.Authorization = 'Bearer ' + kc.token;
-        return Promise.resolve(config)
-    }).catch(() => {
-        kc.login();
-    })
-});
-
-// need to wrap the KC "promise object" into a real Promise object
-const refreshToken = (minValidity = 5) => {
-    return new Promise((resolve, reject) => {
-        kc.updateToken(minValidity)
-            .success(() => resolve())
-            .error(error => reject(error))
-    });
-};*/
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+  contentBase: path.resolve(__dirname, 'public'),
+  compress: true,
+  port: 9000
+},
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [{
+          loader: "html-loader",
+          options: {
+            minimize: true
+          }
+        }]
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: MiniCssExtractPlugin.loader
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        }]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
