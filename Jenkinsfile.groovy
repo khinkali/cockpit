6c8bf97c3193d42516a41752c375797545494473
Rob <brem_robert@hotmail.com>
2018-05-29 19:22:20 +0200

    [REFAC][4] use jenkins lib

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -98,31 +98,3 @@ podTemplate(label: 'mypod', containers: [
         }
     }
 }
-
-void waitUntilReady(String label, String containerName) {
-    def podVersion = ''
-    container('kubectl') {
-        while (podVersion != env.VERSION) {
-            def pods = sh(
-                    script: "kubectl -n test get po -l ${label} --field-selector=status.phase=Running --no-headers",
-                    returnStdout: true
-            ).trim()
-            def podNameLine = pods.split('\n')[0]
-            def startIndex = podNameLine.indexOf(' ')
-            if (startIndex == -1) {
-                return
-            }
-            def podName = podNameLine.substring(0, startIndex)
-            try {
-                def versionString = sh(
-                        script: "kubectl -n test exec ${podName} -c ${containerName} env | grep ^VERSION=",
-                        returnStdout: true
-                ).trim()
-                podVersion = versionString.split('=')[1]
-                echo "podVersion: ${podVersion}"
-            } catch (e) {
-                echo e.getMessage()
-            }
-        }
-    }
-}
