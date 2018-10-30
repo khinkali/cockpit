d6488560ca986b0deeffd8d1506374ea2a5935b2
Developer <developer@developer.com>
2018-10-30 18:51:22 +0100

    use ELM

--- src/Account/Page.elm
+++ src/Account/Page.elm
@@ -13,13 +13,14 @@ import Http exposing (..)
 type alias Model =
     { 
         currencies : List String,
+        selectedCurrency : String,
         error : String
     }
 
 
 init : Env.Model -> Keycloak.Struct -> ( Model, Cmd Msg )
 init env kc =
-    ( Model [] "" 
+    ( Model [] ""  ""
     , Http.send Currencies (Coins.reqCoins env kc)
     )
 
@@ -28,6 +29,7 @@ init env kc =
 
 type Msg
     = Currencies (Result Http.Error (List String))
+    | SelectedCurrs String
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -38,14 +40,15 @@ update msg model =
                     ({model | currencies = value }, Cmd.none)
                 Err _ ->
                     ({model | error = "Can not query coins currency" }, Cmd.none)
-
+        SelectedCurrs curr ->
+            ({model | selectedCurrency = curr}, Cmd.none)
 
 
 --- SUBSCRIPTIONS ---
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.none 
 
 
 ---- VIEW ----
@@ -53,7 +56,12 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    p [] [text model.error]
-
-
+    div [class "select-box-medium"] [
+        select [] <| List.map (\x -> option [] [text x]) model.currencies ,
+        div [class "dropper"] [
+            i [class "far fa-arrow-alt-circle-down"] []
+        ]
+    ]
+    
+    
 ---- FUNCTIONS ----
---
+++ src/Component/Select.elm
@@ -0,0 +1,8 @@
+module Component.Select exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+
